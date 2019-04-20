aeb8154a4acd0da137a286f20ceb4e10c8354147
Pedro Torres <pedrohagatorres@gmail.com>
2019-04-20 18:34:37 -0300

    Minor styling correction and cleanup.

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -14,25 +14,22 @@ class MoviesController < ApplicationController
   end
 
   def create
-    #@movie = Movie.create!(params[:movie]) #did not work on rails 5.
     @movie = Movie.create(movie_params)
     flash[:notice] = "#{@movie.title} was successfully created!"
     redirect_to movies_path
   end
 
   def movie_params
-    params.require(:movie).permit(:title,:rating,:description,:release_date)
+    params.require(:movie).permit(:title, :rating, :description, :release_date)
   end
 
   def edit
     id = params[:id]
     @movie = Movie.find(id)
-    #@movie = Movie.find params[:id]
   end
 
   def update
     @movie = Movie.find params[:id]
-    #@movie.update_attributes!(params[:movie])#did not work on rails 5.
     @movie.update(movie_params)
     flash[:notice] = "#{@movie.title} was successfully updated!"
     redirect_to movie_path(@movie)

198a38824b49bcf177551466f2c0d7e19e956e44
Pedro Torres <pedrohagatorres@gmail.com>
2019-04-20 18:29:22 -0300

    Added 'movies_helper.rb' since it wasn't added in earlier commit.

---
+++ app/helpers/movies_helper.rb
@@ -0,0 +1,16 @@
+module MoviesHelper
+  def sort_link(column, title = nil)
+    title ||= column.titleize
+    direction = (column == sort_column && sort_direction == "asc") ? "desc" : "asc"
+    link_to title, :sort => column, :direction => direction
+  end
+
+  private
+  def sort_column
+    params[:sort] || "name"
+  end
+
+  def sort_direction
+    params[:direction] || "asc"
+  end
+end
